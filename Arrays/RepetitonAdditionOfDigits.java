28b2f42477a571184dbae44dd53a1a7061bd6156
Vanshika Chaudhary <123478862+vanshika241@users.noreply.github.com>
2025-03-29 16:18:21 +0530

    Create RepetitonAdditionOfDigits.java

---
+++ Arrays/RepetitonAdditionOfDigits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleDigit(int n) {
+        // code here
+        int sum = 0;
+        while(n>0 || sum>9){
+            if(n == 0){
+                n  = sum;
+                sum = 0;
+            }
+            sum += n%10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
